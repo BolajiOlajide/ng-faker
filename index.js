4284965c9b806c08aded8dc398b72b0eb272c6c3
Rotimi Babalola <rotimi.babalola@andela.com>
2018-02-15 04:10:59 +0100

    create first fake function (#1)
    * create first fake function
    
    * add jsdoc to classes
    
    - write test for functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+var NgFaker = require('./lib');
+
+// create new instance of faker class
+var ngfaker = new NgFaker();
+
+module.exports = ngfaker;
--- tests/fake.spec.js
+++ tests/fake.spec.js
@@ -1,3 +1,10 @@
-test('fake test', () => {
-  expect(true).toBe(true);
+if (typeof module !== 'undefined') {
+    var faker = require('../index');
+}
+
+describe('Fake: ', function() {
+  test('it returns a string when called', function() {
+    var fake = faker.fake();
+    expect(fake).toBe('fake string');
+  });
 });
