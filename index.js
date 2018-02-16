0086b7afac0f9068d93046f18e0e462a7d0205f8
BolajiOlajide <bolaji.olajide@andela.com>
2018-02-16 15:45:32 +0100

    create random class

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var NgFaker = require('./lib');
 // create new instance of faker class
 var ngfaker = new NgFaker({ locales: require('./lib/locales') });
 
+console.log(ngfaker.random.alphanumeric(6));
+
 module.exports = ngfaker;
