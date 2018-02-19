9a88e9653ddc29ca173a83f6d2c27ea1f18618a0
andela-rbabalola <rotimi.babalola@andela.com>
2018-02-19 12:18:55 +0100

    commit before rebasing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var NgFaker = require('./lib');
 
 // create new instance of faker class
-var ngfaker = new NgFaker({ locales: require('./lib/locales') });
+var ngfaker = new NgFaker({ datasets: require('./lib/data') });
 
 module.exports = ngfaker;
