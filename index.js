7959e2757063f56b2be2c3231ea22178439d84ba
andela-rbabalola <rotimi.babalola@andela.com>
2018-02-19 17:52:14 +0100

    use locales to get data for names

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var NgFaker = require('./lib');
 
 // create new instance of faker class
-var ngfaker = new NgFaker();
+var ngfaker = new NgFaker({ locales: require('./lib/locales') });
 
-console.log(ngfaker.name.firstName(), '****');
+console.log(ngfaker.name.firstName(1), '++++');
 
 module.exports = ngfaker;
