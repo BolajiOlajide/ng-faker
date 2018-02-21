db5e946821d9b6985e60752ab438d9f7e4c6d6cc
Bolaji Olajide <bolaji.olajide@andela.com>
2018-02-21 15:30:56 +0100

    write test for name function (#7)

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+# Added this here so if linting fails, then the script should return a
+# non-zero exit code.
+set -eo pipefail
+
+if ! [ -d "node_modules" ]; then
+    # run npm install to install dependencies
+    yarn install
+fi
+
 # Run a lint check
 yarn lint
 
 # Run unit tests
 NODE_ENV=testing ./node_modules/.bin/jest ./**/*.spec.js
+
+echo ' 💪 All tests passed! 😄 '
+
+# if everything works fine, then ensure the scripts retutn a success code
+exit 0
