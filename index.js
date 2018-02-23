96112f6c1227f4d5fd74ca82563c1a79be3e2f50
andela-rbabalola <rotimi.babalola@andela.com>
2018-02-23 14:37:45 +0100

    final clean up

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ var NgFaker = require('./lib');
 // create new instance of faker class
 var ngfaker = new NgFaker({ locales: require('./lib/locales') });
 
-ngfaker.setLocale('ig');
-
-console.log(ngfaker.name.firstName(), '+++++');
-
 // export the instance of faker created
 module.exports = ngfaker;
