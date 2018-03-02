5deb3a3a2c91aae846b471e7b83d2dc317426cb8
andela-rbabalola <rotimi.babalola@andela.com>
2018-03-02 13:12:04 +0100

    write tests for local governments generation

--- tests/address.spec.js
+++ tests/address.spec.js
@@ -103,4 +103,116 @@ describe('Address:', function () {
             expect(states.length).toBeGreaterThanOrEqual(37);
         });
     });
+
+    describe('Local Government', function () {
+        test('it should return a random local government if no state is passed', function () {
+            var localGovernment = ngfaker.address.localGovernment();
+            expect(address.local_government.all.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+            expect(typeof localGovernment).toEqual('string');
+        });
+
+        test('it should return a random local government from a state', function () {
+            var localGovernment = ngfaker.address.localGovernment('akwa_ibom');
+            expect(address.local_government.akwa_ibom.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('kogi');
+            expect(address.local_government.kogi.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('imo');
+            expect(address.local_government.imo.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('sokoto');
+            expect(address.local_government.sokoto.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('oyo');
+            expect(address.local_government.oyo.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+        });
+
+        test('it should return a random local government if an invalid state is passed', function () {
+            var localGovernment = ngfaker.address.localGovernment('kgkgk');
+            expect(address.local_government.all.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+            expect(typeof localGovernment).toEqual('string');
+        });
+
+        test('it should return a local government for a state with spaces', function () {
+            var localGovernment = ngfaker.address.localGovernment('akwa ibom');
+            expect(address.local_government.akwa_ibom.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('cross river');
+            expect(address.local_government.cross_river.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('federal capital territory');
+            expect(address.local_government.federal_capital_territory.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+        });
+
+        test('it should return a local government if the state has capital letters', function () {
+            var localGovernment = ngfaker.address.localGovernment('Akwa Ibom');
+            expect(address.local_government.akwa_ibom.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('Cross River');
+            expect(address.local_government.cross_river.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+        });
+
+        test('it should return a local government if the state has capital letters and underscores', function () {
+            var localGovernment = ngfaker.address.localGovernment('Akwa_Ibom');
+            expect(address.local_government.akwa_ibom.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+
+            var localGovernment = ngfaker.address.localGovernment('Cross_River');
+            expect(address.local_government.cross_river.indexOf(localGovernment)).toBeGreaterThanOrEqual(0);
+        });
+
+        test('it contains correct (current)  total number of local governments', function () {
+            expect(address.local_government.all.length).toEqual(774);
+        });
+    });
+
+    describe('Local Governments', function () {
+        test('it should return the correct number of local goverments for a state', function () {
+            var localGovernments = ngfaker.address.localGovernments('akwa_ibom');
+            expect(localGovernments.length).toEqual(31);
+            expect(localGovernments.indexOf('Abak')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Okobo')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Obot Akara')).toBeGreaterThanOrEqual(0);
+
+            var localGovernments = ngfaker.address.localGovernments('lagos');
+            expect(localGovernments.length).toEqual(20);
+            expect(localGovernments.indexOf('Agege')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Oshodi - Isolo')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Ikeja')).toBeGreaterThanOrEqual(0);
+
+            var localGovernments = ngfaker.address.localGovernments('kano');
+            expect(localGovernments.length).toEqual(44);
+            expect(localGovernments.indexOf('Dala')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Kano Municipal')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Dawakin Kudu')).toBeGreaterThanOrEqual(0);
+        });
+
+        test('it should return a random array of local governments if no state is passed', function () {
+            var localGovernments = ngfaker.address.localGovernments();
+            expect(Array.isArray(localGovernments)).toBeTruthy();
+            expect(localGovernments.length).toBeGreaterThan(0);
+        });
+
+        test('it should return a random array of local governments if an invalid state is passed', function () {
+            var localGovernments = ngfaker.address.localGovernments('lclc');
+            expect(Array.isArray(localGovernments)).toBeTruthy();
+            expect(localGovernments.length).toBeGreaterThan(0);
+        });
+
+        test('it should return local governments if the state has spaces', function () {
+            var localGovernments = ngfaker.address.localGovernments('akwa ibom');
+            expect(localGovernments.length).toEqual(31);
+            expect(localGovernments.indexOf('Abak')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Okobo')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Obot Akara')).toBeGreaterThanOrEqual(0);
+        });
+
+        test('it should return local governments if the state has capital letters', function () {
+            var localGovernments = ngfaker.address.localGovernments('Akwa Ibom');
+            expect(localGovernments.length).toEqual(31);
+            expect(localGovernments.indexOf('Abak')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Okobo')).toBeGreaterThanOrEqual(0);
+            expect(localGovernments.indexOf('Obot Akara')).toBeGreaterThanOrEqual(0);
+        });
+    });
 });
