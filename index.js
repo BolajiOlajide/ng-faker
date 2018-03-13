6313ef32586c733a732cc5e65b610db26b11b03b
Ikechi Michael <mykehell123@gmail.com>
2018-03-13 17:48:00 +0100

    feat: addLocale method

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var NgFaker = require('./lib');
 
+const {
+  en,
+  yo,
+  ig,
+  ha
+} = require('./lib/locales');
+
 // create new instance of faker class
-var ngfaker = new NgFaker({ locales: require('./lib/locales') });
+var ngfaker = new NgFaker({
+  locales: {
+    en
+  }
+});
+
+ngfaker.addLocale('yo', yo);
+ngfaker.addLocale('ig', ig);
+ngfaker.addLocale('ha', ha);
+
 
 // export the instance of faker created
 module.exports = ngfaker;
