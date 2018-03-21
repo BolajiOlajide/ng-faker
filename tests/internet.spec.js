e36d272cc22ab78fdad1d228112b24af758934ef
rotimi-babalola <rotimi.babalola@andela.com>
2018-03-21 19:15:07 +0100

    add tests for email generation

---
+++ tests/internet.spec.js
@@ -0,0 +1,23 @@
+if (typeof module !== 'undefined') {
+  var ngfaker = require('../index');
+}
+
+describe('Internet: ', function () {
+  describe('Email', function () {
+    // I got this from this Stackoverflow thread - https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    var EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    test('it should generate a valid email', function () {
+      var email = ngfaker.internet.email();
+      expect(EMAIL_REGEX.test(email)).toBeTruthy();
+    });
+
+    test('it should generate a valid email if parameters are passed in', function () {
+      var email = ngfaker.internet.email('john', 'doe', 'aol.com');
+      expect(EMAIL_REGEX.test(email)).toBeTruthy();
+      expect(email.includes('john')).toBeTruthy();
+      expect(email.includes('doe')).toBeTruthy();
+      expect(email.includes('aol.com')).toBeTruthy();
+    });
+  });
+});
